01ab78ae48a4c091ba6ee386bb24df437c584a1f
liweilei <liweilei@awinic.com.cn>
2019-05-16 13:56:32 +0800

    [Project]: aw8996 [Descrip]: smartpa driver [Type   ]: update [Modify ]: 1. remove porting document            2. add COPYING [Author ]: liweilei

--- aw8896_reg.h
+++ aw8896_reg.h
@@ -1,3 +1,10 @@
+/*
+ * This program is free software; you can redistribute  it and/or modify it
+ * under  the terms of  the GNU General  Public License as published by the
+ * Free Software Foundation;  either version 2 of the  License, or (at your
+ * option) any later version.
+ */
+
 #ifndef _AW8896_REG_H_
 #define _AW8896_REG_H_
 
